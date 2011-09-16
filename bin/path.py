94026a7ae19e19e0e7c870c1f001fcf62d9ac4a9
cholin <cholin@fu-berlin.de>
2011-09-16 17:51:26 +0200

    added fcgi bin

--- bin/path.py
+++ bin/path.py
@@ -1,4 +1,5 @@
 import sys
 from os import path
 
-sys.path.insert(0, path.join(path.dirname(path.abspath(__file__)), '..', 'src'))
+_srcPath = path.join(path.dirname(path.abspath(__file__)), '..', 'src')
+sys.path.insert(0, _srcPath)
