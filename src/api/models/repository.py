b09fa6ad050818b61795f4ea88106e6bf2d71ed5
cholin <cholin@fu-berlin.de>
2011-09-25 19:54:29 +0200

    revert - every user has its own ssh account

--- src/api/models/repository.py
+++ src/api/models/repository.py
@@ -33,8 +33,7 @@ class Repository(db.Model):
 
     @hybrid_property
     def cloneUrl(self):
-        return 'ssh://%s@%s/%s/%s' % (\
-                    str(current_app.config['SSH_USER']),
+        return 'ssh://$USER@%s/%s/%s' % (\
                     str(current_app.config['SERVER_NAME_GIT']),
                     self.owner.username,
                     self.name
